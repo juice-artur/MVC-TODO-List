0ad70b967e665485aa87b644c40eb032950e24fb
asasin.lol10@gmail.com <JJ-STU_DIO[D[D[-[C[C[C>
2021-10-28 21:57:28 +0300

    -Big mode

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,7 @@ namespace todo_rest_api
         {
 
             services.AddControllers();
-            services.AddSingleton<TodoItemList>();
+            services.AddSingleton<TodoItemListService>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "todo_rest_api", Version = "v1" });

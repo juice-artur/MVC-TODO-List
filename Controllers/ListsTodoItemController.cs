2704fb63120dfe30adf1db846f356934c54312f9
Artur Dzhus <asasin.lol10@gmail.com>
2021-10-29 16:55:46 +0300

    Worked version our AOI

--- Controllers/ListsTodoItemController.cs
+++ Controllers/ListsTodoItemController.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using todo_rest_api.Models;
 
@@ -6,19 +9,54 @@ namespace todo_rest_api.Controllers
 {
     [Route("api/[controller]/{listId}/tasks")]
     [ApiController]
-    public class ListsTodoItemController
+    public class ListsTodoItemController: ControllerBase
     {
-        private TodoItemListService _todoItemService;
+        private readonly TodoItemListService _todoItemService;
         public ListsTodoItemController(TodoItemListService service)
         {
             _todoItemService = service;
         }
-
-
+        
+        [HttpGet("")]
+        public Dictionary<string, List<TodoItem>> GetTasks()
+        {
+            return _todoItemService.GetTasks();
+        }
         [HttpGet("{id}")]
         public ActionResult<TodoItem> GetTask(int id)
         {
             return _todoItemService.GetTask(id);
+        }
+
+        [HttpPost("")]
+        public ActionResult<TodoItem> PostTask(int listId, TodoItem task)
+        {
+            _todoItemService.CreateTaskInRepository(listId, task);
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult PutTask(int id, TodoItem task)
+        {
+            _todoItemService.PutTodoItem(id, task);
+
+            return Ok();
+        }
+        
+        [HttpPatch("{id}")]
+        public IActionResult PatchTask(int id, TodoItem task)
+        {
+            _todoItemService.PatchTodoItem(id, task);
+
+            return Ok();
+        }
+
+        
+        [HttpDelete("{id}")]
+        public ActionResult<TodoItem> DeleteTask(int id)
+        {
+            _todoItemService.DeleteTodoItem(id);
+
+            return Ok();
         }
         
     }
--- Controllers/TodoListsController.cs
+++ Controllers/TodoListsController.cs
@@ -12,94 +12,88 @@ namespace todo_rest_api.Controllers
     [ApiController]
     public class TodoListController : ControllerBase
     {
-        private TodoItemListService todoItemService;
+        private readonly TodoItemListService _todoItemService;
 
 
         public TodoListController(TodoItemListService service)
         {
-            this.todoItemService = service;
+            this._todoItemService = service;
         }
 
 
-        [HttpGet("")]
-        public ActionResult<IEnumerable<RepositoryTodoItem>> GetTodoItems()
-        {
-            return todoItemService.GetAllRepository();
-        }
+        // [HttpGet]
+        // public ActionResult<IEnumerable<RepositoryTodoItem>> GetTodoItems()
+        // {
+        //     return _todoItemService.GetAllRepository();
+        // }
 
 
-        [HttpGet("list")]
+        [HttpGet]
         public ActionResult<RepositoryTodoItem> GetTodoListById(int listId)
         {
-            return todoItemService.GetTodoRepository(listId);
+            return _todoItemService.GetTodoRepository(listId);
         }
 
 
-        [HttpPost("")]
+        [HttpPost]
         public ActionResult<RepositoryTodoItem> CreateTodoList(RepositoryTodoItem todoItem)
         {
-            todoItemService.AddRepository(todoItem);
+            _todoItemService.AddRepository(todoItem);
 
             return Created($"api/todolist/{todoItem.Id}", todoItem);
         }
 
 
-        [HttpDelete("list")]
+        [HttpDelete]
         public ActionResult<RepositoryTodoItem> DeleteTodoItemById(int listId)
         {
-            todoItemService.RemoveRepo(listId);
+            _todoItemService.RemoveRepo(listId);
 
             return Ok();
         }
-
-        [HttpGet("tasks")]
-        public ActionResult<Dictionary<string, List<TodoItem>>> GetTasks()
-        {
-            return todoItemService.GetTasks();
-        }
-
+        //
+        // [HttpGet("tasks")]
+        // public ActionResult<Dictionary<string, List<TodoItem>>> GetTasks()
+        // {
+        //     return _todoItemService.GetTasks();
+        // }
         
-        [HttpGet("task")]
-        public ActionResult<TodoItem> GetTask(int taskId)
-        {
-            return todoItemService.GetTask(taskId);
-        }
 
 
-        [HttpPost("task")]
-        public ActionResult<TodoItem> PostTask(int listId, TodoItem task)
-        {
-            todoItemService.CreateTaskInRepository(listId, task);
-
-            return Created($"api/task/{listId}/{task.Id}", task);
-        }
-
-
-        [HttpPut("task")]
-        public IActionResult PutTask(int listId, int taskId, TodoItem task)
-        {
-            todoItemService.PutTodoItem(taskId, task);
-
-            return Ok();
-        }
+        // [HttpPost("task")]
+        // public ActionResult<TodoItem> PostTask(int listId, TodoItem task)
+        // {
+        //     _todoItemService.CreateTaskInRepository(listId, task);
+        //
+        //     return Created($"api/task/{listId}/{task.Id}", task);
+        // }
 
 
-        [HttpPatch("task")]
-        public IActionResult PatchTask(int taskId, TodoItem task)
-        {
-            todoItemService.PatchTodoItem(taskId, task);
+        // [HttpPut("task")]
+        // public IActionResult PutTask(int listId, int taskId, TodoItem task)
+        // {
+        //     _todoItemService.PutTodoItem(taskId, task);
+        //
+        //     return Ok();
+        // }
 
-            return Ok();
-        }
+        //
+        // [HttpPatch("task")]
+        // public IActionResult PatchTask(int taskId, TodoItem task)
+        // {
+        //     _todoItemService.PatchTodoItem(taskId, task);
+        //
+        //     return Ok();
+        // }
 
 
-        [HttpDelete("task")]
-        public ActionResult<TodoItem> DeleteTaskById(int taskId)
-        {
-            todoItemService.DeleteTodoItem(taskId);
-
-            return Ok();
-        }
+        // [HttpDelete("task")]
+        // public ActionResult<TodoItem> DeleteTaskById(int taskId)
+        // {
+        //     _todoItemService.DeleteTodoItem(taskId);
+        //
+        //     return Ok();
+        // }
 
     }
 }
